e2d52805f2e7b2445c9495434bf71a8b3589720a
prnvme <work.pranavdhekane@gmail.com>
2025-11-08 21:41:00 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,19 +24,16 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Pranav Dhekane",
+  title: "Web Developer",
+  email: "work.pranavdhekane@gmail.com",
+  gitHub: "prnvme",
+  linkedIn: "pranavdhekane",
+  twitter: "pranavdhekane",
 };
 
-const primaryColor = "#4E567E";
-const secondaryColor = "#D2F1E4";
+const primaryColor = "#123fff";
+const secondaryColor = "green";
 
 const App = () => {
   return (
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -31,30 +31,26 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "PsyConnect – A Mental Health Awareness and Support Platform",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "A web-based platform offering mental health support with an AI chatbot, myth-busting section, and yoga recommendations for well-being.",
+    url: "https://psyconnect-chat.vercel.app/hero",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Pune Metro Transport Analysis",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "A data-driven web project analyzing Pune Metro transport data to reveal key insights and travel trends.",
+    url: "https://punemetroanalysis.netlify.app/",
   },
   {
-    title: "My Resume Site",
+    title: "CarFinder",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
-  },
-  {
-    title: "GitHub Codespaces and github.dev",
-    description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "A frontend-only React.js web application that helps users find cars based on specific search criteria and preferences.",
+    url: "https://carfinder-web.netlify.app/",
   },
 ];
+
+
 
 const Portfolio = () => {
   return (
